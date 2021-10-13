30fb77a56cfaf846f12546a70a2f555c37551b94
Fakorede Damilola <dammy.fakorede@gmail.com>
2021-10-13 22:08:30 +0100

    worked on the confirm modal on liquidity page

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -10,14 +10,28 @@ import {
   Heading,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./../../components/ColorModeSwitcher";
+import ConfirmModal from "../AddLiquidity/modals/ConfirmModal";
 
 const App = () => {
+  const [show,setShow] = React.useState(true)
+  
   return (
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <VStack spacing={8}>
           <Heading>Swap page</Heading>
           <Button variant="brand"> Swap </Button>
+          <ConfirmModal 
+            title="confirm"
+            amount="3463"
+            from="RGP"
+            to="BUSD"
+            fromPrice="0.5464"
+            toPrice="1.3838"
+            fromDeposited="7.3838"
+            toDeposited="1.3838"
+            poolShare="0.12"
+            />
         </VStack>
       </Grid>
     </Box>
