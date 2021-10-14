a179969121a0d332bd7f58f03711fd8157186d83
Eberechi <eberewill@gmail.com>
2021-10-14 02:11:38 +0100

    fixed design iregularity and refactored component

--- src/components/Modals/Test.tsx
+++ src/components/Modals/Test.tsx
@@ -9,51 +9,30 @@ import { ColorModeSwitcher } from "./../../components/ColorModeSwitcher";
 import TransactionStateModal from "../../components/Modals/TransactionStateModal";
 import { TrxState } from "../../components/Modals/TransactionStateModal";
 
-interface IState {
-    trxWaiting: boolean;
-    trxSuccess: boolean;
-    trxFailed: boolean;
-}
 
 const Test = () => {
 
-
-    const { isOpen, onOpen, onClose } = useDisclosure()
-
-
-
-    const [waitIsopen, setwaitIsopen] = useState(false)
-
-    const [successIsopen, setSuccessIsopen] = useState(false)
-
-    const [failIsopen, setfailIsopen] = useState(false)
-
-
-
-
-
-
     return (
         <div>
-            <Button variant="brand" onClick={() => setwaitIsopen(true)}> Show  Waiting Transaction </Button>
-            {" "}
-            <Button variant="brand" onClick={() => setSuccessIsopen(true)} > SHow Transaction Success </Button>
 
-            {" "}
-            <Button variant="brand" onClick={() => setfailIsopen(true)} > Show Transaction Fail </Button>
 
             <TransactionStateModal
                 message={"Supplying 4.4311 RGP and 0.03434 USDT"}
                 URLNetwork="google.com"
-                trxState={TrxState.TransactionFailed} isOpen={failIsopen} onClose={onClose} />
+                typeOfModal={"Fail transaction modal"}
+                trxState={TrxState.TransactionFailed} />
+
             <TransactionStateModal
                 message={"Supplying 4.4311 RGP and 0.03434 USDT"}
                 URLNetwork="google.com"
-                trxState={TrxState.TransactionSuccessful} isOpen={successIsopen} onClose={onClose} />
+                typeOfModal={"transaction Success Modal"}
+                trxState={TrxState.TransactionSuccessful} />
+
             <TransactionStateModal
                 message={"Supplying 4.4311 RGP and 0.03434 USDT"}
                 URLNetwork="google.com"
-                trxState={TrxState.WaitingForConfirmation} isOpen={waitIsopen} onClose={onClose} />
+                typeOfModal={"Waiting comfirmation Modal"}
+                trxState={TrxState.WaitingForConfirmation} />
         </div>
     )
 }
--- src/components/Modals/TransactionStateModal.tsx
+++ src/components/Modals/TransactionStateModal.tsx
@@ -25,10 +25,10 @@ export interface IProps {
     message: string;
     trxState: TrxState;
     URLNetwork?: string;
-    isOpen: boolean;
-    onClose: () => void;
+    typeOfModal: string
+
 }
-const TransactionStateModal: React.FC<IProps> = ({ message, trxState, URLNetwork, onClose, isOpen }) => {
+const TransactionStateModal: React.FC<IProps> = ({ message, URLNetwork, trxState, typeOfModal }) => {
 
 
     const bgColour = useColorModeValue("#FFFFFF", "#15202B");
@@ -36,181 +36,121 @@ const TransactionStateModal: React.FC<IProps> = ({ message, trxState, URLNetwork
     const smallTxtColour = useColorModeValue("#999999", "#DCE5EF");
     const closeBtnColour = useColorModeValue("#666666", "#DCE5EF");
     const closeButtonBgColour = useColorModeValue("#319EF6", "#008DFF");
+    const successBgColour = useColorModeValue("#22BB33", "#75F083");
+    const errorBgColour = useColorModeValue("#CC334F", "#FF3358");
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
 
 
-    const WaitingForConfirmationRender = (): JSX.Element => {
+    return (
 
-        return (
+        <>
+
+            <Button variant="brand" onClick={onOpen}>
+                {typeOfModal}
+            </Button>
+
+            {console.log("transction state is ", trxState)}
             <Modal isOpen={isOpen} onClose={onClose} isCentered>
                 <ModalOverlay />
-                <ModalContent bg={bgColour} color="#fff" borderRadius="20px" width="70%">
+                <ModalContent bg={bgColour} color="#fff" borderRadius="6px"
+                    paddingBottom="15px" width="95vw">
                     <ModalCloseButton
                         bg="none"
-                        border="0px"
                         color={closeBtnColour}
                         cursor="pointer"
                         _focus={{ outline: 'none' }}
                         onClick={onClose}
-
-                    />
-                    <ModalBody align="center">
-                        <Spinner
-                            thickness="4px"
-                            speed="0.65s"
-                            emptyColor="transparent"
-                            color="#319EF6"
-                            size="xl"
-                            width="100px"
-                            height="100px"
-                        />
-                        <Text fontSize="18px" fontWeight="normal" py={4} color={textColour}>
-                            Waiting for Confirmation
-                        </Text>
-                        <Text fontSize="16px" fontWeight="bold" color={textColour}>
-                            {message}
-                        </Text>
-                        <Text size="xs" color={smallTxtColour} mt={'20px'} >
-                            Go to your wallet to confirm this
-                        </Text>
-
-                    </ModalBody>
-                </ModalContent>
-            </Modal>
-        )
-
-    }
-
-
-    const TransactionSuccessfulRender = (): JSX.Element => {
-        return (
-            <Modal isOpen={isOpen} onClose={onClose} isCentered>
-                <ModalOverlay />
-                <ModalContent bg={bgColour} color="#fff" borderRadius="20px" width="70%">
-                    <ModalCloseButton
-                        bg="none"
-                        border="0px"
-                        color={closeBtnColour}
-                        cursor="pointer"
-                        _focus={{ outline: 'none' }}
-                        onClick={onClose}
-
-                    />
-                    <ModalBody align="center" my={2}>
-                        <Circle size="80px" background="#68C18A" my={3}>
-                            <Circle size="70px" background={bgColour} my={3}>
-
-                                <CheckIcon fontSize="40px" color="#68C18A" />
-                            </Circle>
-                        </Circle>
-                        <Text fontSize="18px" fontWeight="normal" color={textColour}>
-                            Transaction Succesful
-                        </Text>
-                        <Text
-                            py={3}
-                            fontSize="14px"
-                            fontWeight="normal"
-                            color="#008DFF"
-                        >
-
-                            {URLNetwork && (
-                                <Link href="https://etherscan.io" isExternal>
-                                    View on Etherscan
-                                </Link>
-                            )}
-                        </Text>
-                        <Button
-                            width="100%"
-
-                            border="0"
-                            py={6}
-                            mt={3}
-                            background={closeButtonBgColour}
-                            color="#FFFFFF"
-                            cursor="pointer"
-                            onClick={onClose}
-                        >
-                            Close
-                        </Button>
-                    </ModalBody>
-                </ModalContent>
-            </Modal>
-        )
-
-    }
-
-    const TransactionFailedRender = (): JSX.Element => {
-        return (
-            <Modal isOpen={isOpen} onClose={onClose} isCentered>
-                <ModalOverlay />
-                <ModalContent bg={bgColour} color="#fff" borderRadius="20px" width="70%">
-                    <ModalCloseButton
-                        bg="none"
-                        border="0px"
-                        color={closeBtnColour}
-                        cursor="pointer"
-                        _focus={{ outline: 'none' }}
-                        onClick={onClose}
-
+                        border={'1px solid'}
+                        size={'sm'}
+                        mt={3}
+                        mr={3}
+                        p={'7px'}
                     />
                     <ModalBody align="center" my={2}>
 
+                        {trxState === TrxState.WaitingForConfirmation ?
+                            <Spinner
+                                thickness="4px"
+                                speed="0.53s"
+                                emptyColor="transparent"
+                                color="#319EF6"
+                                size="xl"
+                                width="100px"
+                                height="100px"
+                                my={10}
+                            /> : trxState === TrxState.TransactionSuccessful ?
+                                <Circle size="90px" background={successBgColour} my={8}>
+                                    <Circle size="80px" background={bgColour} my={3}>
+                                        <CheckIcon fontSize="40px" color={successBgColour} />
+                                    </Circle>
+                                </Circle> :
+                                trxState === TrxState.TransactionFailed ?
+                                    <Circle size="90px" background={errorBgColour} my={8}>
+                                        <Circle size="80px" background={bgColour} my={3}>
+                                            <CloseIcon width="30px" height="30" color={errorBgColour} />
+                                        </Circle>
+                                    </Circle> : null}
 
-                        <Circle size="80px" background="#FF3358" my={3}>
 
-                            <Circle size="70px" background={bgColour} my={3}>
-                                <CloseIcon width="30px" height="30" color="#FF3358" />
-                            </Circle>
-                        </Circle>
-                        <Text fontSize="18px" fontWeight="normal" color={textColour}>
-                            Transaction Not Successful
+                        <Text fontSize="20px" fontWeight="normal" color={textColour}>
+                            {trxState === TrxState.TransactionSuccessful ?
+                                "Transaction Succesful" :
+                                trxState === TrxState.WaitingForConfirmation ?
+                                    " Waiting for Confirmation" :
+                                    trxState === TrxState.TransactionFailed ?
+                                        " Transaction Not Successful" : null}
                         </Text>
-                        <Text
-                            py={3}
-                            fontSize="14px"
-                            fontWeight="normal"
-                            color="#008DFF"
-                        >
 
-                            <a href="#" target="_blank">
-                                Retry
-                            </a>
+                        {trxState === TrxState.WaitingForConfirmation ?
+                            <Text fontSize="16px" py={5} fontWeight="bold" color={textColour}>
+                                {message}
+                            </Text> :
+                            trxState === TrxState.TransactionFailed ?
+                                <Text
+                                    py={3}
+                                    fontSize="14px"
+                                    fontWeight="normal"
+                                    color="#008DFF">
+                                    <a href="#" target="_blank">
+                                        Retry
+                                    </a>
 
-                        </Text>
-                        <Button
-                            width="100%"
+                                </Text> : trxState === TrxState.TransactionSuccessful ? <Text
+                                    py={3}
+                                    fontSize="14px"
+                                    fontWeight="normal"
+                                    color="#008DFF">
 
-                            border="0"
-                            py={6}
-                            mt={3}
-                            background={closeButtonBgColour}
-                            color="#FFFFFF"
-                            cursor="pointer"
-                            onClick={onClose}
-                        >
-                            Close
-                        </Button>
+                                    {URLNetwork && (<Link href={`https://${URLNetwork}`} isExternal> View on Etherscan </Link>)}
+                                </Text> : null}
+
+                        {trxState === TrxState.WaitingForConfirmation ?
+                            <Text size="12px" color={smallTxtColour}  >
+                                Go to your wallet to confirm this
+                            </Text> :
+                            <Button
+                                variant="brand"
+                                padding="24px 0"
+                                width="100%"
+                                isFullWidth
+                                boxShadow="none"
+                                border="0"
+                                mt={3}
+                                background={closeButtonBgColour}
+                                color="#FFFFFF"
+                                cursor="pointer"
+                                onClick={onClose}
+                            >
+                                Close
+                            </Button>}
                     </ModalBody>
                 </ModalContent>
             </Modal>
-        )
+        </>
+    )
 
-    }
-
-
-    switch (trxState) {
-        case TrxState.WaitingForConfirmation:
-            return WaitingForConfirmationRender()
-            break;
-        case TrxState.TransactionSuccessful:
-            return TransactionSuccessfulRender();
-            break;
-        case TrxState.TransactionFailed:
-            return TransactionFailedRender();
-            break;
-        default:
-            return null;
-    }
 }
+
 
 export default TransactionStateModal
