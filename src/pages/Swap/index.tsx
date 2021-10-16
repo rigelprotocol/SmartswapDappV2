c2a2489a86274ae2c642dcb4fae621e6af7dc1a7
Fakorede Damilola <dammy.fakorede@gmail.com>
2021-10-16 20:23:23 +0100

    created the wallet modal

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -10,7 +10,7 @@ import {
   Heading,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./../../components/ColorModeSwitcher";
-import ConfirmModal from "../AddLiquidity/modals/ConfirmModal";
+import WalletModal from "../../components/Navbar/modals/walletModal";
 
 const App = () => {
   const [show,setShow] = React.useState(true)
@@ -21,17 +21,7 @@ const App = () => {
         <VStack spacing={8}>
           <Heading>Swap page</Heading>
           <Button variant="brand"> Swap </Button>
-          <ConfirmModal 
-            title="confirm"
-            amount="3463"
-            from="RGP"
-            to="BUSD"
-            fromPrice="0.5464"
-            toPrice="1.3838"
-            fromDeposited="7.3838"
-            toDeposited="1.3838"
-            poolShare="0.12"
-            />
+          <WalletModal />
         </VStack>
       </Grid>
     </Box>
