3b0d8afab963e57bda44fdb77572dab4f48b5ca4
Goodness Chris-Ugari <goodnesschrisugari@yahoo.com>
2021-10-20 16:56:07 +0100

    Updated Set price UI

--- src/pages/Swap/SetPrice.tsx
+++ src/pages/Swap/SetPrice.tsx
@@ -38,12 +38,60 @@ const SetPrice = () => {
       >
         {isMobileDevice ? (
           <>
-            <Box mx={4} mt={8} w={['100%', '100%', '45%', '29.5%']} mb={4}>
-
-            </Box>
 
             <Box mx={4} w={['100%', '100%', '45%', '29.5%']} mb={4}>
               <ShowDetails />
+            </Box>
+
+            <Box
+              mx={4} w={['100%', '100%', '45%', '29.5%']} mb={4}
+              borderColor={borderColor} borderWidth="1px" borderRadius="6px" pl={3} pr={3}
+              h="500px"
+              >
+              <SwapSettings/>
+              <From/>
+              <Box borderRadius="6px" border="1px" borderColor={borderColor} mb={4} mt={4} h="102px">
+                <To/>
+              </Box>
+
+              <Flex>
+                <Text fontSize="14px" color={setColor} mr={2}>
+                  Set Price
+                </Text>
+                <ExclamationIcon/>
+              </Flex>
+
+              <Input placeholder="0.00" size="lg" borderRadius={4} borderColor={borderColor}/>
+
+              <Flex  mt={5}>
+                <Center borderColor={setColor} borderColor={iconColor} borderWidth="1px" borderRadius={4} w="20px" h="20px">
+                  <VectorIcon/>
+                </Center>
+                <Spacer/>
+                <Text fontSize="14px" mr={2} color={textColorOne}>
+                  1 RGP = 1.34566 USDT
+                </Text>
+                <ExclamationIcon/>
+              </Flex>
+
+              <Box mt={5}>
+                <Button
+                  w="100%"
+                  borderRadius="6px"
+                  border={lightmode ? '2px' : 'none'}
+                  borderColor={borderColor}
+                  h="48px"
+                  p="5px"
+                  color={color}
+                  bgColor={buttonBgcolor}
+                  fontSize="18px"
+                  boxShadow={lightmode ? 'base' : 'lg'}
+                  _hover={{ bgColor: buttonBgcolor }}
+                >
+                  Enter Percentage
+                </Button>
+              </Box>
+
             </Box>
 
             <Box mx={4} w={['100%', '100%', '45%', '29.5%']} mb={4}>
