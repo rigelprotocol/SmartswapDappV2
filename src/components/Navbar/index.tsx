436095e7a542a479ca1c89f9fa181e63bca41ab0
princeadeyeye <muizadeyeye@gmail.com>
2021-10-23 07:20:36 +0100

    nav fix

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,51 +1,108 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
 import {
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
+  Flex,
+  Spacer,
+  Box,
+  Img,
+  Text,
+  Stack,
+  Link,
   Button,
-} from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
+  Image,
+  useColorModeValue,
+  useMediaQuery,
+} from '@chakra-ui/react';
+import { NavLink } from 'react-router-dom';
+import { ExternalLinkIcon, HamburgerIcon } from '@chakra-ui/icons';
+import { ColorModeSwitcher } from '../ColorModeSwitcher';
+import SocialMedia from './SocialMedia';
+import DappsDropdown from './DappsDropdown';
+import WalletConnection from './WalletConnection';
+import SwapDropdown from './SwapDropdown';
+import LightLogo from './../../assets/logo/logo-light.svg';
+import DarkLogo from './../../assets/logo/logo-dark.svg';
+import MetamaskLogo from './../../assets/metamaskLogo.png';
+import MobileNavDrawer from './MobileNavDrawer';
+import NetworkConnector from '../NetworkConnector';
 
 const Nav = ({ to, label }: { to: string; label: string }) => (
   <NavLink
     to={to}
     activeStyle={{
-      color: "#319EF6",
+      color: '#319EF6',
     }}
   >
     {label}
   </NavLink>
 );
 
-function SwapDropdown() {
+const Index = () => {
+  const [isMobileDevice] = useMediaQuery('(max-width: 750px)');
+  const Logo = useColorModeValue(LightLogo, DarkLogo);
+  const mobileNavColor = useColorModeValue('#FFFFFF', '#15202B');
+  const mobileNavBorderColor = useColorModeValue('#DEE5ED', '#324D68');
   return (
-    <Menu>
-      <MenuButton
-        variant="ghost"
-        as={Button}
-        transition="all 0.2s"
-        rightIcon={<ChevronDownIcon />}
-        fontWeight={200}
-        _focus={{color: "#319EF6"}}
-      >
-        Swap
-      </MenuButton>
-      <MenuList>
-        <MenuItem _focus={{color: "#319EF6"}}>
-          <Nav label="Straight Swap" to="/swap" />
-        </MenuItem>
-        <MenuItem _focus={{color: "#319EF6"}}>
-          <Nav label="Auto-Time" to="/auto-time" />
-        </MenuItem>
-        <MenuItem _focus={{color: "#319EF6"}}>
-          <Nav label="Set Price" to="/set-price" />
-        </MenuItem>
-      </MenuList>
-    </Menu>
-  );
-}
+    <Flex px={6} py={2} boxShadow="sm">
+      {isMobileDevice ? (
+        <>
+          <Flex w="100%" justifyContent="space-between" h="10">
+            <Box mr={6}>
+              <Img src={Logo} />
+            </Box>
+            <MobileNavDrawer />
+          </Flex>
+          <Flex
+            h="70px"
+            zIndex="2"
+            position="fixed"
+            left={0}
+            bottom={0}
+            justify="space-between"
+            alignItems="center"
+            borderTop="1px"
+            borderColor={mobileNavBorderColor}
+            w="100%"
+            bgColor={mobileNavColor}
+            mr={4}
+          >
+            <Flex ml={4}>
+              <WalletConnection />
+            </Flex>
+            <Flex mr={4}>
+              <ColorModeSwitcher />
+              <SocialMedia />
+            </Flex>
+          </Flex>
+        </>
+      ) : (
+        <>
+          <Flex h="10">
+            <Box mr={6}>
+              <Img src={Logo} />
+            </Box>
+            <DappsDropdown />
 
-export default SwapDropdown;
+            <Flex w="350px" h="10" align="center" justify="space-between">
+              <SwapDropdown />
+              <Nav label="Liquidity" to="/pool" />
+              <Nav label="Farming" to="/farming" />
+              <Link href="https://rigelprotocol.com" isExternal>
+                Analytics <ExternalLinkIcon mx="2px" />
+              </Link>
+            </Flex>
+          </Flex>
+          <Spacer />
+
+          <Flex h="8" justify="flex-end">
+            <NetworkConnector />
+            <WalletConnection />
+            <SocialMedia />
+            <ColorModeSwitcher />
+          </Flex>
+        </>
+      )}
+    </Flex>
+  );
+};
+
+export default Index;
