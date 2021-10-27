2cfbc8c6f427b4d16c3542ee59b7431dbe9f2edc
Eberechi <eberewill@gmail.com>
2021-10-27 13:35:29 +0100

    fixed connectWallet for injected and bscconnector

--- src/connectors/WalletProvider.tsx
+++ src/connectors/WalletProvider.tsx
@@ -1,22 +1,43 @@
 import React, { useEffect, useState } from 'react'
-import { injected } from '.'
+import { injected , bscConnector,connectorsByName, walletconnect} from '.'
 import { useWeb3React } from '@web3-react/core'
 
 function WalletProvider({ children }: { children: JSX.Element }): JSX.Element  {
   const { active: networkActive, error: networkError, activate: activateNetwork } = useWeb3React()
   const [loaded, setLoaded] = useState(false)
+ 
   useEffect(() => {
-    injected
-      .isAuthorized()
+
+    if( injected){
+      injected
+       .isAuthorized()
       .then((isAuthorized) => {
         setLoaded(true)
         if (isAuthorized && !networkActive && !networkError) {
           activateNetwork(injected)
         }
-      })
-      .catch(() => {
+      }) .catch(() => {
         setLoaded(true)
       })
+    } if(bscConnector){
+      bscConnector
+      .isAuthorized()
+      .then((isAuthorized) => {
+        setLoaded(true)
+        if (isAuthorized && !networkActive && !networkError) {
+          activateNetwork(bscConnector)
+        }
+      }) .catch(() => {
+        setLoaded(true)
+      })
+    }
+    if (!injected || !bscConnector){
+        setLoaded(true)
+        if ( !networkActive && !networkError) {
+          activateNetwork(walletconnect)
+        }
+      
+    }
   }, [activateNetwork, networkActive, networkError])
   if (loaded) {
     return children
