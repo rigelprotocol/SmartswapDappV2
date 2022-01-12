a2fcf3088d93b2a9d91bbbd33160937a18f2e973
Goodness Chris-Ugari <goodnesschrisugari@yahoo.com>
2022-01-12 09:02:52 +0100

    fix error

--- src/utils/hooks/useRGPPrice.ts
+++ src/utils/hooks/useRGPPrice.ts
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { ethers } from 'ethers';
-import {smartSwapLPTokenPoolOne} from "../../utils/Contracts"
+import { smartSwapLPTokenPoolOne } from "../../utils/Contracts";
+import { SMARTSWAPLP_TOKEN1ADDRESSES } from "../../utils/addresses";
 
 export const useRGPPrice = () => {
   const { chainId } = useWeb3React();
@@ -10,15 +11,15 @@ export const useRGPPrice = () => {
   useEffect(() => {
     const getRGPprice = async () => {
       try {
-        const RGPBUSDToken = await smartSwapLPTokenPoolOne();
+        const RGPBUSDToken = await smartSwapLPTokenPoolOne(SMARTSWAPLP_TOKEN1ADDRESSES[chainId as number]);
         const reserves = await RGPBUSDToken.getReserves();
         setRGPPrice(ethers.utils.formatUnits(reserves[0].mul(10000).div(reserves[1]), 4));
-        console.log(RGPPrice)
       } catch (error) {
         setRGPPrice(0);
         console.log(error)
       }
     };
+    getRGPprice();
   }, [chainId]);
   return [RGPPrice];
 }
