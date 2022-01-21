6637a5f9a81efd26324acd7dc25fbaee432a5427
Kayode Okunlade <okunladekayode@gmail.com>
2022-01-21 10:06:42 +0100

    bg:create new pair with large amount

--- src/utils/calculateSlippageAmount.ts
+++ src/utils/calculateSlippageAmount.ts
@@ -1,7 +1,8 @@
 import { Fraction, Percent } from '@uniswap/sdk-core';
 
 export function calculateSlippageAmount(value: any, slippage: number): string {
-  const fraction = new Fraction(slippage, 100)
+  const userSlippage = slippage > 0 ? slippage : 1
+  const fraction = new Fraction(userSlippage, 100)
     .multiply(new Fraction(value, 1))
     .divide(new Fraction(100, 1))
     .subtract(new Fraction(value, 1));
