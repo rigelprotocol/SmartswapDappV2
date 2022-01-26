aed7d794af813015ab9637ccc388fa8d71681378
Godswill Adigwe <gcadigwe@gmail.com>
2022-01-26 19:33:25 +0100

    Oasis Emerald Mainnet and Testnet Implementation

--- src/utils/constants/chains.ts
+++ src/utils/constants/chains.ts
@@ -1,42 +1,45 @@
-import BNBLOGO from '../../assets/BNB.svg';
-import ETHLOGO from '../../assets/eth.svg';
-import MATICLOGO from '../../assets/Matic.svg';
+import BNBLOGO from "../../assets/BNB.svg";
+import ETHLOGO from "../../assets/eth.svg";
+import MATICLOGO from "../../assets/Matic.svg";
 export const SupportedChainSymbols: { [key: string]: string } = {
-  '1': 'ETH',
-  '3': 'ETH',
-  '4': 'ETH',
-  '56': 'BNB',
-  '97': 'BNB',
-  '137': 'MATIC',
-  '80001': 'MATIC',
+  "1": "ETH",
+  "3": "ETH",
+  "4": "ETH",
+  "56": "BNB",
+  "97": "BNB",
+  "137": "MATIC",
+  "80001": "MATIC",
+  "42261": "ROSE",
 };
 
 export const SupportedChainName: { [key: string]: string } = {
-  '1': 'Ether',
-  '3': 'Ether',
-  '4': 'Ether',
-  '56': 'Binance Coin',
-  '97': 'Binance Coin',
-  '137': 'Matic',
-  '80001': 'Matic',
+  "1": "Ether",
+  "3": "Ether",
+  "4": "Ether",
+  "56": "Binance Coin",
+  "97": "Binance Coin",
+  "137": "Matic",
+  "80001": "Matic",
+  "42261": "ROSE",
 };
 
 export const SupportedChainLogo: { [key: string]: string } = {
-  '1': ETHLOGO,
-  '3': ETHLOGO,
-  '4': ETHLOGO,
-  '56': BNBLOGO,
-  '97': BNBLOGO,
-  '137': MATICLOGO,
-  '80001': MATICLOGO,
+  "1": ETHLOGO,
+  "3": ETHLOGO,
+  "4": ETHLOGO,
+  "56": BNBLOGO,
+  "97": BNBLOGO,
+  "137": MATICLOGO,
+  "80001": MATICLOGO,
 };
 
 export const WrappedSymbols: { [key: string]: string } = {
-  '1': 'WETH',
-  '3': 'WETH',
-  '4': 'WETH',
-  '56': 'WBNB',
-  '97': 'WBNB',
-  '80001': 'WMATIC',
-  '137': 'WMATIC',
+  "1": "WETH",
+  "3": "WETH",
+  "4": "WETH",
+  "56": "WBNB",
+  "97": "WBNB",
+  "80001": "WMATIC",
+  "137": "WMATIC",
+  "42261": "wROSE",
 };
