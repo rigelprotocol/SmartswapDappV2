ee0dd4108a9cd46af54a92b1607c4da108e6401e
Goodness Chris-Ugari <goodnesschrisugari@yahoo.com>
2022-01-28 14:02:30 +0100

    RGP special pool v2

--- src/pages/FarmingV2/index.tsx
+++ src/pages/FarmingV2/index.tsx
@@ -173,7 +173,7 @@ export function Index() {
 
   const showProject = () => {
     changeVersion(
-      "https://docs.google.com/forms/d/e/1FAIpQLSdJGAuABrJd6d0WSprUWB140we9hGqa-IwIbonx9ZJhxN2zsg/viewform", 
+      "https://docs.google.com/forms/d/e/1FAIpQLSdJGAuABrJd6d0WSprUWB140we9hGqa-IwIbonx9ZJhxN2zsg/viewform",
       true);
 
   }
@@ -643,7 +643,7 @@ export function Index() {
         my={4}
         isFitted={isMobileDevice ? true : false}
       >
- 
+
         <TabList borderBottom={0}>
           <Tab
             isDisabled={switchTab}
@@ -845,7 +845,7 @@ export function Index() {
               margin='10px 16px'
             >
               <option value={1}>V1</option>
-              <option value={4}>V2</option>
+              <option value={5}>V2</option>
             </Select>
           </Tab>
           <Tab
@@ -1138,6 +1138,77 @@ export function Index() {
             </Flex>
           </TabPanel>
           <TabPanel padding='0px'></TabPanel>
+          <TabPanel padding='0px'>
+            <Flex
+              justifyContent='center'
+              alignItems='center'
+              rounded='lg'
+              mb={4}
+            >
+              <Box
+                bg='#120136'
+                minHeight='89vh'
+                w={["100%", "100%", "100%"]}
+                background={
+                  mode === LIGHT_THEME
+                    ? "#FFFFFF !important"
+                    : mode === DARK_THEME
+                    ? "#15202B !important"
+                    : "#FFFFFF !important"
+                }
+                rounded='lg'
+              >
+                <Box mx='auto' w={["100%", "100%", "100%"]} pb='70px'>
+                  <Flex
+                    alignItems='center'
+                    justifyContent='space-between'
+                    px={4}
+                    py={4}
+                    background={
+                      mode === DARK_THEME
+                        ? "#213345"
+                        : mode === LIGHT_THEME
+                        ? "#F2F5F8"
+                        : "#F2F5F8 !important"
+                    }
+                    color={
+                      mode === LIGHT_THEME
+                        ? "#333333"
+                        : mode === DARK_THEME
+                        ? "#F1F5F8"
+                        : "#333333"
+                    }
+                    w={["100%", "100%", "100%"]}
+                    align='left'
+                    border={
+                      mode === LIGHT_THEME
+                        ? "1px solid #DEE5ED !important"
+                        : mode === DARK_THEME
+                        ? "1px solid #324D68 !important"
+                        : "1px solid #324D68"
+                    }
+                    display={{ base: "none", md: "flex", lg: "flex" }}
+                  >
+                    <Text>Deposit</Text>
+                    <Text>Earn</Text>
+                    <Text>APY</Text>
+                    <Text>Total Liquidity</Text>
+                    <Text />
+                  </Flex>
+                  {FarmData.contents.map((content: any, index: number) =>
+                    index === 0 ? (
+                      <YieldFarm
+                        farmDataLoading={farmDataLoading}
+                        content={content}
+                        key={content.pid}
+                        wallet={wallet}
+                      />
+                    ) : null
+                  )}
+                </Box>
+              </Box>
+            </Flex>
+          </TabPanel>
         </TabPanels>
       </Tabs>
     </Box>
