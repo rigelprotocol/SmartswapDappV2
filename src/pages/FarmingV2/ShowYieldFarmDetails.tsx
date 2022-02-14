809ea2efe628fe5e7c85b01f2f3b56e2dd3e6ebb
JoshuaRotimi <62189959+JoshuaRotimi@users.noreply.github.com>
2022-02-14 13:55:40 +0100

    unstake bug fix

--- src/pages/FarmingV2/ShowYieldFarmDetails.tsx
+++ src/pages/FarmingV2/ShowYieldFarmDetails.tsx
@@ -707,7 +707,7 @@ const ShowYieldFarmDetails = ({
         } else if (val === "RGP-BNB" || val === "RGP-USDT") {
           await tokensWithdrawal(2);
         } else if (
-          val === "RBG-BUSD" ||
+          val === "RGP-BUSD" ||
           val === "MATIC-RGP" ||
           val === "RGP-ROSE"
         ) {
