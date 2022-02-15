a5b30464bcc13011bd349379cb0759f63277f865
Godswill Adigwe <gcadigwe@gmail.com>
2022-02-15 15:05:23 +0100

    fix toast decimal

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,74 +1,77 @@
+export const NetworkContextName = "NETWORK";
 
-
-export const NetworkContextName = 'NETWORK';
-
-export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-
+export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 export const approveAbi = [
-    {
-        inputs: [
-            { internalType: 'address', name: 'spender', type: 'address' },
-            { internalType: 'uint256', name: 'amount', type: 'uint256' },
-        ],
-        name: 'approve',
-        outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
-        stateMutability: 'nonpayable',
-        type: 'function',
-    },
+  {
+    inputs: [
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
 ];
 
 export const allowanceAbi = [
-    {
-        inputs: [
-            { internalType: 'address', name: 'owner', type: 'address' },
-            { internalType: 'address', name: 'spender', type: 'address' },
-        ],
-        name: 'allowance',
-        outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-        stateMutability: 'view',
-        type: 'function',
-    },
+  {
+    inputs: [
+      { internalType: "address", name: "owner", type: "address" },
+      { internalType: "address", name: "spender", type: "address" },
+    ],
+    name: "allowance",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
 ];
 
 export const getDeadline = (deadlineInMinutes: number) =>
-    Math.floor(new Date().getTime() + Number(deadlineInMinutes));
+  Math.floor(new Date().getTime() + Number(deadlineInMinutes));
 
-export const getOutPutDataFromEvent = async (tokenAddress: string | undefined, eventsArray: any[]) => {
-    const duplicateArray: any[] = [];
-    eventsArray.map(event => {
-        if (event.address.toLowerCase() === tokenAddress?.toLowerCase()) {
-            duplicateArray.push(event);
-        }
-    });
-
-    if (duplicateArray.length != 0) {
-        const convertedInput = (
-            parseInt(duplicateArray[0].data, 16) /
-            10 ** 18
-        ).toFixed(7);
-        return convertedInput;
+export const getOutPutDataFromEvent = async (
+  tokenAddress: string | undefined,
+  eventsArray: any[],
+  decimals?: number
+) => {
+  const duplicateArray: any[] = [];
+  const decimal = decimals ? decimals : 16;
+  const seconddecimal = decimals ? decimals : 18;
+  eventsArray.map((event) => {
+    if (event.address.toLowerCase() === tokenAddress?.toLowerCase()) {
+      duplicateArray.push(event);
     }
+  });
+
+  if (duplicateArray.length != 0) {
+    const convertedInput = (
+      parseInt(duplicateArray[0].data, decimal) /
+      10 ** seconddecimal
+    ).toFixed(7);
+    return convertedInput;
+  }
 };
 
 export const getInPutDataFromEvent = (
-    tokenAddress: string | undefined,
-    eventsArray: any [],
-    fromAmount: string | undefined,
+  tokenAddress: string | undefined,
+  eventsArray: any[],
+  fromAmount: string | undefined
 ) => {
-    const duplicateArray: any[] = [];
-    eventsArray.map(event => {
-        if (event.address.toLowerCase() === tokenAddress?.toLowerCase()) {
-            duplicateArray.push(event);
-        }
-    });
-
-    if (duplicateArray.length !== 0) {
-        const convertedInput = parseInt(duplicateArray[0].data, 16) / 10 ** 18;
-
-        if (parseFloat(convertedInput) !== parseFloat(fromAmount)) {
-            return convertedInput.toFixed(7);
-        }
-        return fromAmount;
+  const duplicateArray: any[] = [];
+  eventsArray.map((event) => {
+    if (event.address.toLowerCase() === tokenAddress?.toLowerCase()) {
+      duplicateArray.push(event);
     }
+  });
+
+  if (duplicateArray.length !== 0) {
+    const convertedInput = parseInt(duplicateArray[0].data, 16) / 10 ** 18;
+
+    if (parseFloat(convertedInput) !== parseFloat(fromAmount)) {
+      return convertedInput.toFixed(7);
+    }
+    return fromAmount;
+  }
 };
