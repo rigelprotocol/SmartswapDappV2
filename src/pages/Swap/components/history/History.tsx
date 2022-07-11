9a940eeec52f9c898ef515bcac2ceacb0ada7814
Fakorede Damilola <dammy.fakorede@gmail.com>
2022-07-11 11:42:31 +0100

    changes

--- src/pages/Swap/components/history/History.tsx
+++ src/pages/Swap/components/history/History.tsx
@@ -35,7 +35,7 @@ const History = () => {
 
   useEffect(
     () => {
-  setSocket(io("https://autoperiod.rigelprotocol.com"));//https://autoperiod.rigelprotocol.com
+  setSocket(io("https://autoswap-server.herokuapp.com"));//https://autoswap-server.herokuapp.com
   
     },
     []
@@ -48,7 +48,7 @@ const History = () => {
   const [showMarketHistory, setShowMarketHistory] = useState(false);
   const [notification, setNotification] = useState(0);
   const [address, setAddress] = useState("");
-  const [URL, setURL] = useState("https://autoperiod.rigelprotocol.com")//https://autoperiod.rigelprotocol.com
+  const [URL, setURL] = useState("https://autoswap-server.herokuapp.com")//https://autoswap-server.herokuapp.com
   const [showOrder, setShowOrder] = useState(false);
   const [type, setType] = useState("");
 
