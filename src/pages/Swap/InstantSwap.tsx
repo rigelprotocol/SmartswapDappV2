35fbd51b01d0922a7edb6de25afa3488175fd0d7
Fakorede Damilola <dammy.fakorede@gmail.com>
2022-10-28 08:46:37 +0100

    changes

--- src/pages/Swap/InstantSwap.tsx
+++ src/pages/Swap/InstantSwap.tsx
@@ -520,7 +520,7 @@ const handleMaxInput = async () => {
    
     if (response && value) {
      
-      const response = await fetch(`https://autoswap-server.herokuapp.com/auto/instant`, {
+      const response = await fetch(`http://localhost:7000/auto/instant`, {
         method: "POST",
         mode: "cors",
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
